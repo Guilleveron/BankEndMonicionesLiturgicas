66cb622cb57cf5b60129dd9719c3286e221a5ac0
Guillermo Veron <guilleveron.gvf.gv@gmail.com>
2021-10-15 17:20:09 -0300

    cambio de bd

--- config/conexion.js
+++ config/conexion.js
@@ -1,9 +1,9 @@
 var mysql=require("mysql");
 var con= mysql.createPool({
     host:'us-cdbr-east-04.cleardb.com',
-    user:'b3dc391cc851fb',
-    password:'08fcee7d',
-    database:'heroku_dfe49c234591b59'
+    user:'bd839da3b0d485',
+    password:'7ee796ce',
+    database:'heroku_ae4cdccedabc381'
 }
 );
 con.getConnection(
